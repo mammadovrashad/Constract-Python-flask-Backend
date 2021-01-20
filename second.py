2daec32cce6e7a88f58ac0cf614fd62d3577f499
Resad Memmedov <talehg2932@gmail.com>
2021-01-20 10:37:59 +0400

    delete second

--- second.py
+++
@@ -1,6 +0,0 @@
-class User:
-    def __init__(self,_name,_surname):
-        self.Name=_name
-        self.Surname=_surname
-    def fullName(self):
-        print(f'sizin adiniz: {self.Name} sizin soyadiniz:{self.Surname} ')
