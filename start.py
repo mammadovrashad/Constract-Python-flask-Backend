26d829a99f990a3c50367e6ea416730a72e0b3a0
Resad Memmedov <talehg2932@gmail.com>
2021-02-03 18:13:46 +0400

    crudlarin yazilmasi

--- start.py
+++ start.py
@@ -1,10 +1,20 @@
+from enum import unique
+import re
 from flask import Flask,render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
-
+import os
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/data.db"
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
+
+registration=[]
+# modellerin yazilmasi
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     writingBox1=db.Column(db.Text)
@@ -13,6 +23,17 @@ class Post(db.Model):
     writingBox4=db.Column(db.Text)
     writingBox5=db.Column(db.Text)
     writingBox6=db.Column(db.Text)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    yourName=db.Column(db.String(50),nullable='False')
+    yourEmailAddress=db.Column(db.String(255),nullable='False')
+    contactPhone=db.Column(db.Integer,nullable='False')
+    yourProfession=db.Column(db.String,nullable='False')
+    request=db.Column(db.Text,nullable='False')
+
+
+
+
 
 titleHeader='header'
 titleAbout='about'
@@ -20,23 +41,35 @@ titlePages='pages'
 titleService='service'
 titleSingleServices='singleServices'
 titlePortfolio='portfolio'
+
+# header sehifesinin yazilmasi
 @app.route("/")
 def header():
     return render_template('header.html',title1=titleHeader)
+
+# about sehifenin yazilmasi
 @app.route("/about")
 def about():
     posts=Post.query.all()
     return render_template('about.html',title2=titleHeader,postList=posts)
-@app.route("/pages")
+
+# contact hissenin yazilmasi
+@app.route("/contact")
 def pages():
     return render_template('pages.html',title3=titleHeader)
+
+# service hissenin yazilmasi
 @app.route("/service")
 def service():
     return render_template('service.html',title4=titleHeader)
+
+# singleService hissenin yazilmasi
 @app.route("/singleServices")
 def singleServices():
     posts=Post.query.all()
     return render_template('singleServices.html',title5=titleHeader,postList=posts)
+
+# portfolio hissenin yazilmasi
 @app.route("/portfolio")
 def portfolio():
     return render_template('portfolio.html',title6=titleHeader)
@@ -44,7 +77,7 @@ def portfolio():
 
 
 
-
+# about/add routunun yazilmasi
 @app.route("/about/add",methods=["GET","POST"])
 def aboutAdd():
     if request.method=='POST':
@@ -59,10 +92,13 @@ def aboutAdd():
         db.session.commit()
         redirect("/all")
     return render_template('admin/aboutAdd.html')
+
+# admin hissede all routunun yazilmasi
 @app.route("/all")
 def All():
     posts=Post.query.all()
-    return render_template('admin/all.html',postList=posts)
+    users=User.query.all()
+    return render_template('admin/all.html',postList=posts,userList=users)
 @app.route("/about/delete/<int:id>")
 def aboutDelete(id):
     postId=Post.query.get(id)
@@ -90,5 +126,33 @@ def aboutUpdate(id):
         db.session.commit()
         return redirect("/all")
     return render_template('admin/aboutUpdate.html',post=post)
+
+@app.route("/registration",methods=['POST','GET'])
+def Resgistration():
+    if request.method=='POST':
+        user=User(yourName=request.form['name'],yourEmailAddress=request.form["email"],contactPhone=request.form["contact"],
+        yourProfession=request.form["profession"], request=request.form["request"])
+        db.session.add(user)
+        db.session.commit()
+        return redirect("/mail")
+    return render_template('header.html')
+
+@app.route("/mail")
+def Mail():
+    users=User.query.all()
+    return render_template('admin/mail.html',userList=users)
+@app.route("/registration/delete/<int:id>")
+def deleteReg(id):
+    userId=User.query.get(id)
+    db.session.delete(userId)
+    db.session.commit()
+    return redirect("/all")
+@app.route("/mail/delete/<int:id>")
+def deleteMail(id):
+    userId=User.query.get(id)
+    db.session.delete(userId)
+    db.session.commit()
+    return redirect("/mail")
 if __name__=='__main__':
     app.run(debug=True)
+    manager.run()
